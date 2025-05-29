4e4f4215a111b490763a434a2a446ee6bfa0f323
Ryder Raymond <117836603+RyderRaymond@users.noreply.github.com>
2025-05-29 16:51:51 -0400

    Now we take in the program name for later use

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use std::env::set_current_dir;
+use std::env;
 use std::io::{self, Read, Write};
 use std::process::{exit, Command};
-use termion::cursor;
 use termion::raw::IntoRawMode;
 //use std::env;
 
@@ -24,7 +23,7 @@ impl RushTerminal {
             io::stdout().flush();
             write!(io::stdout(), "$ ");
             io::stdout().flush().unwrap_or_else(|error| {
-                panic!("Could not write shell prompt: {error:?}");
+                panic!("{name}Could not write shell prompt: {error:?}", name=self.program_name);
             });
 
             // let mut command = String::new();
@@ -60,7 +59,7 @@ impl RushTerminal {
                 false => &[]
             };
 
-            if is_builtin_command(command_name, argument_vector) {
+            if self.is_builtin_command(command_name, argument_vector) {
                 continue;
             }
 
@@ -131,58 +130,65 @@ impl RushTerminal {
         return command;
     }
 
+
+    fn usage_error(&mut self, message: &str) {
+        eprintln!("{name}: {m}", name=self.program_name, m=message);
+    }
+
+    fn builtin_exit(&mut self, argument_vector: &[&str]) {
+        if argument_vector.len() == 0 {
+            exit(0);
+        }
+
+        let exit_val: i32 = match argument_vector[0].trim().parse::<i32>() {
+            Ok(num) => num,
+            Err(_) => {
+                self.usage_error(&format!("exit {}: numeric argument required", argument_vector[0]));
+                1
+            }
+        };
+
+        exit(exit_val);
+    }
+
+    fn builtin_cd(&mut self, argument_vector: &[&str]) {
+        let directory = match argument_vector.get(0) {
+            None => "/", //we'll have to implement $HOME later
+            Some(dir) => dir
+        };
+
+        match env::set_current_dir(directory) {
+            Ok(_) => (),
+            Err(error) => eprintln!("{}", error)
+        };
+    }
+
+    fn is_builtin_command(&mut self, command: &str, argument_vector: &[&str]) -> bool {
+        match command {
+            "exit" => {
+                self.builtin_exit(argument_vector);
+                return true;
+            }
+            "cd" => {
+                self.builtin_cd(argument_vector);
+                return true;
+            }
+            _ => false
+        }
+    }
 }
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let prog_name = match args.get(0) {
+        Some(name) => name,
+        None => "rush" //default
+    };
+
     let mut terminal = RushTerminal {
-        program_name : String::from("rush"),
+        program_name : String::from(prog_name),
     };
 
     terminal.repl_loop();
 }
 
-fn is_builtin_command(command: &str, argument_vector: &[&str]) -> bool {
-    match command {
-        "exit" => {
-            builtin_exit(argument_vector);
-            return true;
-        }
-        "cd" => {
-            builtin_cd(argument_vector);
-            return true;
-        }
-        _ => false
-    }
-}
-
-fn usage_error(message: &str) {
-    eprintln!("rush: {}", message);
-}
-
-fn builtin_exit(argument_vector: &[&str]) {
-    if argument_vector.len() == 0 {
-        exit(0);
-    }
-
-    let exit_val: i32 = match argument_vector[0].trim().parse::<i32>() {
-        Ok(num) => num,
-        Err(_) => {
-            usage_error(&format!("exit {}: numeric argument required", argument_vector[0]));
-            1
-        }
-    };
-
-    exit(exit_val);
-}
-
-fn builtin_cd(argument_vector: &[&str]) {
-    let directory = match argument_vector.get(0) {
-        None => "/", //we'll have to implement $HOME later
-        Some(dir) => dir
-    };
-
-    match set_current_dir(directory) {
-        Ok(_) => (),
-        Err(error) => eprintln!("{}", error)
-    };
-}
